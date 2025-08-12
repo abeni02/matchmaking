0b2f411f316639488d9021fb3744631dbb1d847c
abeni02 <118209856+abeni02@users.noreply.github.com>
2025-08-12 16:27:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,8 +17,8 @@ import datetime
 from motor.motor_asyncio import AsyncIOMotorClient
 
 # Constants for limits
-MAX_ACTIVE_USERS = 600
-MAX_CONCURRENT_MATCHES = 300
+MAX_ACTIVE_USERS = 2000
+MAX_CONCURRENT_MATCHES = 500
 
 # Locks for synchronization
 user_data_lock = asyncio.Lock()
